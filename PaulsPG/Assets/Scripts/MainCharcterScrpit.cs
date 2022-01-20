b15c185e2f3e8a7ee426f15e3285cc38b0b721d9
PKerins97 <paulkerins123@gmail.com>
2022-01-20 20:42:30 +0000

    Imported assests such as enviroment probs and extra charcters coded a little bit of movement for the main character Signed-off-by: PKerins97 <paulkerins123@gmail.com>

--- PaulsPG/Assets/Scripts/MainCharcterScrpit.cs
+++ PaulsPG/Assets/Scripts/MainCharcterScrpit.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainCharcterScrpit : MonoBehaviour
+public  class MainCharcterScrpit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+
+    public GameObject MainCharacter;
+    
     void Update()
     {
-        
+        if(Input.GetButtonDown("Forwards"))
+        {
+            MainCharacter.GetComponent<Animator>().Play("Run");
+        }
+        if (Input.GetButtonDown("Backwards"))
+        {
+            MainCharacter.GetComponent<Animator>().Play("Backwards");
+        }
+        if (Input.GetButtonDown("Left"))
+        {
+            MainCharacter.GetComponent<Animator>().Play("Left");
+        }
+        if (Input.GetButtonDown("Right"))
+        {
+            MainCharacter.GetComponent<Animator>().Play("Right");
+        }
     }
 }
