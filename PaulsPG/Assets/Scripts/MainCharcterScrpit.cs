518e3a474a1f0ba983683d932cbfd4142672c601
PKerins97 <paulkerins123@gmail.com>
2022-03-01 15:13:35 +0000

    Created scripts for stats of characters and starting script for picking up objects Signed-off-by: PKerins97 <paulkerins123@gmail.com>

--- PaulsPG/Assets/Scripts/MainCharcterScrpit.cs
+++ PaulsPG/Assets/Scripts/MainCharcterScrpit.cs
@@ -3,16 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public  class MainCharcterScrpit : MonoBehaviour
+public class MainCharcterScrpit : MonoBehaviour
 {
     private float current_speed;
     private float BACKWARDS_SPEED = 1, RUNNING_SPEED = 5;
     private float turning_speed = 90;
-    private float mouse_sesitivity_x = 0.5f;
+    private float mouse_sesitivity_x = 0.05f;
     Animator char_animation;
-    private bool isGrounded= true;
+    private bool isGrounded = true;
     PlayerCameraScript my_camera;
     private Rigidbody rigg;
+
+
+
 
 
     public GameObject MainCharacter;
@@ -23,16 +26,16 @@ public  class MainCharcterScrpit : MonoBehaviour
         my_camera = GetComponentInChildren<PlayerCameraScript>();
         my_camera.you_belong_to(this);
         rigg = GetComponent<Rigidbody>();
-        
-;    }
-    
+
+        ; }
+
     void Update()
     {
         char_animation.SetBool("running_forward", false);
         char_animation.SetBool("walking_backwards", false);
         char_animation.SetBool("jumping", false);
-        
-       
+
+
 
 
 
@@ -42,31 +45,32 @@ public  class MainCharcterScrpit : MonoBehaviour
         turn(Input.GetAxis("Horizontal"));
         adjust_camera(Input.GetAxis("Vertical"));
 
-        if (Input.GetKey(KeyCode.Space) && isGrounded )
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
             rigg.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
             char_animation.SetBool("jumping", true);
             char_animation.SetBool("jump_landing", false);
             isGrounded = false;
         }
-        if (Input.GetKey(KeyCode.Q)) {
-            char_animation.SetBool("punch1", true);
-        }
+       
+       
+
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         print("collision");
-        if(collision.gameObject.tag == "floor")
-            
+        if (collision.gameObject.tag == "floor")
+
         {
             print("floor");
             char_animation.SetBool("jump_landing", true);
             isGrounded = true;
-            
+
         }
-       
-        
+
+
     }
 
     private void adjust_camera(float vertical_adjustment)
@@ -74,12 +78,12 @@ public  class MainCharcterScrpit : MonoBehaviour
         my_camera.adjust_vertical_angle(vertical_adjustment);
     }
 
-   
+
 
     private void turn(float mouse_turn_value_x)
     {
         transform.Rotate(Vector3.up, mouse_sesitivity_x * mouse_turn_value_x * Time.deltaTime);
-        if(Mathf.Abs(mouse_turn_value_x)>0.2f)char_animation.SetBool("walking_backwards", true);
+        if (Mathf.Abs(mouse_turn_value_x) > 0.2f) char_animation.SetBool("walking_backwards", true);
     }
 
     private void turn_left()
@@ -95,7 +99,7 @@ public  class MainCharcterScrpit : MonoBehaviour
 
     private void move_backward()
     {
-        transform.position -= BACKWARDS_SPEED * transform.forward * Time.deltaTime;      
+        transform.position -= BACKWARDS_SPEED * transform.forward * Time.deltaTime;
         char_animation.SetBool("walking_backwards", true);
     }
 
@@ -108,11 +112,12 @@ public  class MainCharcterScrpit : MonoBehaviour
     {//move in frame rate independant using s = u*t
         transform.position += current_speed * transform.forward * Time.deltaTime;
         char_animation.SetBool("running_forward", true);
-       
+
     }
 
     private bool should_move_forward()
     {
         return Input.GetKey(KeyCode.W);
     }
+   
 }
