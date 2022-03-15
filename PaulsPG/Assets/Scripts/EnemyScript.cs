7bdd3fe63a3c8b7ded7f71e6f13695048a5392a4
STUDENT Paul Kerins <Paul.Kerins@students.ittralee.ie>
2022-03-15 15:55:56 +0000

    fixed enemy movement added iDamageable script Signed-off-by: STUDENT Paul Kerins <Paul.Kerins@students.ittralee.ie>

--- PaulsPG/Assets/Scripts/EnemyScript.cs
+++ PaulsPG/Assets/Scripts/EnemyScript.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyScript : MonoBehaviour
+public class EnemyScript : MonoBehaviour,IDamageable
 {
+
+    enum Enemy_States { Idle, Move_to_Target, Attack};
+
+    Enemy_States isCurrently = Enemy_States.Idle;
+
 
     public Transform target;
     public float speed = 1f;
@@ -11,8 +16,8 @@ public class EnemyScript : MonoBehaviour
     public float howclose;
     private float dist;
     private Rigidbody enemy;
-   
-
+    
+    internal int maxHealth = 60;
 
     // Start is called before the first frame update
     void Start()
@@ -25,30 +30,60 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
         dist = Vector3.Distance(target.position, transform.position);
-        
-
-        if(dist <= howclose)
+        switch (isCurrently)
         {
-            
-            enemy_animation.SetBool("running_forwards", true);
-            Vector3 fromMetoTarget = target.position - transform.position;
-            fromMetoTarget = new Vector3(fromMetoTarget.x, 0, fromMetoTarget.z);
-            Vector3 direction = fromMetoTarget.normalized;
-            transform.position += speed * direction * Time.deltaTime;
-            transform.LookAt(transform.position + direction);
+            case Enemy_States.Idle:
 
-            if(dist <= 1.5f)
-            {
-                
-            }
+
+                if (dist <= howclose)
+                {
+
+                    enemy_animation.SetBool("running_forwards", true);
+
+                    isCurrently = Enemy_States.Move_to_Target;
+
+                }
+
+
+                break;
+
+            case Enemy_States.Move_to_Target:
+
+                Vector3 fromMetoTarget = target.position - transform.position;
+                fromMetoTarget = new Vector3(fromMetoTarget.x, 0, fromMetoTarget.z);
+                Vector3 direction = fromMetoTarget.normalized;
+                transform.position += speed * direction * Time.deltaTime;
+                transform.LookAt(transform.position + direction);
+
+                if (dist < 1.5f)
+                    isCurrently = Enemy_States.Attack;
+
+                break;
+
+            case Enemy_States.Attack:
+                enemy_animation.SetBool("running_forwards", false);
+                enemy_animation.SetBool("attacking",true);
+                IDamageable taregtScript =  target.GetComponent<IDamageable>();
+                if (taregtScript != null)
+                {
+                    taregtScript.takeDamage(20);
+                }
+
+                break;
+
+
+
+
+
+           
 
 
         }
 
 
     }
+   
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Sword")
@@ -59,5 +94,8 @@ public class EnemyScript : MonoBehaviour
         }
     }
 
-    
+    public void takeDamage(int amountOfDamage)
+    {
+        print("Hit");
+    }
 }
--- PaulsPG/Assets/Scripts/IDamageable.cs
+++ PaulsPG/Assets/Scripts/IDamageable.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStats : CharacterStats
+interface IDamageable 
 {
-    
+    void takeDamage(int amountOfDamage);
 }
